498a906044aff8aa3d6adf7e43abe365edbcf5ee
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-03-21 11:43:43 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RootLayoutComponent} from "./components/header/RootLayoutComponent.tsx";
+// import {CustomerPage} from "./pages/CustomerPage.tsx";
+// import {DashboardPage} from "./pages/DashboardPage.tsx";
+import {NotFoundPage} from "./pages/NotFoundPage.tsx";
+// import {ItemPage} from "./pages/ItemPage.tsx";
+// import {OrderPage} from "./pages/OrderPage.tsx";
+
+function App() {
+
+    const routes = createBrowserRouter([
+        {
+            path: '',
+            element : <RootLayoutComponent/>,
+            children : [
+            //     { path : '', element : <DashboardPage/>},
+            //     { path : '/customers', element : <CustomerPage/>},
+            //     { path : '/items', element : <ItemPage/>},
+            //     { path : '/orders', element : <OrderPage/>},
+            // ]
+        },
+        {
+            path: "*",
+            element: <NotFoundPage/>
+        }
+    ])
+
+    return (
+        <>
+            <RouterProvider router={routes} />
+        </>
+    )
+}
+
+export default App
---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
+// import CustomerSlice from "../reducers/CustomerSlice.tsx";
+// import ItemSlice from "../reducers/ItemSlice.tsx";
+// import OrderSlice from "../reducers/OrderSlice.tsx";
+
+export const store = configureStore({
+    reducer: {
+        // customer: CustomerSlice,
+        // item: ItemSlice,
+        // order: OrderSlice,
+    },
+});
